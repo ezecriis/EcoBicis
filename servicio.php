de664824a51f5e9059af3691719e06a7ce756119
Cris <cr.ledesma@hotmail.com>
2020-07-22 20:23:03 -0300

    Latest changes servicio

--- servicio.php
+++ servicio.php
@@ -52,10 +52,8 @@
         <ul>
           <li><a href="index.php">Home</a></li>
           <li><a href="about.php">Sobre nosotros</a></li>
-          <li class="active"><a href="services.php">Services</a></li>
+          <li class="active"><a href="">Services</a></li>
           <li><a href="portfolio.php">Portfolio</a></li>
-          <!-- <li><a href="team.php">Team</a></li> -->
-          <!-- <li><a href="blog.php">Blog</a></li> -->
           <li><a href="contact.php">Contacto</a></li>
           <li class="drop-down"><a href="#">Loguin</a>
             <ul>
