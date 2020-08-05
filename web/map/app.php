049a1992634b697e9bbb6161a1a0f22de7074557
Cris <cr.ledesma@hotmail.com>
2020-08-05 15:31:33 -0300

    Added the folder map and pass recuperation

---
+++ web/map/app.php
@@ -0,0 +1,38 @@
+<?php
+
+  // Archivo de Conexión a la Base de Datos 
+  include('conexion.php');
+
+  // Listamos las direcciones con todos sus datos (lat, lng, dirección, etc.)
+  $result = mysqli_query($con, "SELECT * FROM bicicletero");
+
+  // Creamos una tabla para listar los datos 
+  echo "<div class='table-responsive'>";
+
+  echo "<table class='table table-dark'>
+          <thead class='thead-dark'>
+            <tr>
+              <th scope='col'>Nombre</th>
+              <th scope='col'>Dirección</th>
+              <th scope='col'>Cantidad</th>
+              <th scope='col'>Disponibles</th>
+              <th scope='col'>País</th>
+            </tr>
+            </thead>
+            <tbody>";
+
+  while ($row = mysqli_fetch_array($result)) {
+      echo "<tr>";
+      echo "<td scope='col'>" . $row['nombre'] . "</td>";
+      echo "<td scope='col'>" . preg_replace('/\\\\u([\da-fA-F]{4})/', '&#x\1;', $row['direccion']) . "</td>";
+      echo "<td scope='col'>" . $row['stock'] . "</td>";
+      echo "<td scope='col'>" . $row['disponibles'] . "</td>";
+      echo "<td scope='col'>" . $row['pais'] . "</td>";
+      echo "</tr>";
+  }
+  echo "</tbody></table>";
+  echo "</div>";
+
+  mysqli_close($con);
+
+?> 
