388a924361d17983e363fa19e50d266c251436d8
Cris <cr.ledesma@hotmail.com>
2020-08-06 11:07:58 -0300

    Added the session_start

--- portfolio.php
+++ portfolio.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<?php
+session_start();
+// session_set_cookie_params(60); // 1ms de inactividad
+//if (empty($_SESSION['nombre'])) {
+//    echo "INICIA SESION";
+//}
+?>
 <html lang="en">
 
 <head>
@@ -57,12 +64,25 @@
           <li><a href="servicio.php">Services</a></li>
           <li class="active"><a href="portfolio.php">Portfolio</a></li>
           <li><a href="contact.php">Contacto</a></li>
-          <li class="drop-down"><a href="#">Login</a>
-            <ul>
-              <li><a href="web/registro.php">Registrarce</a></li>
-              <li><a href="web/login.php">Iniciar sesion</a></li>
-            </ul>
-          </li>
+          <?php
+          if (!empty($_SESSION['nombre'])) {
+            echo "<li class='drop-down'><a href='#'>" . $_SESSION['nombre'] . "</a>";
+            echo "<ul>";
+            echo "<li><a href='web/micuenta.php'>Mi Cuenta</a></li>";
+            echo "<li><a href='web/reserva.php'>Reserva</a></li>";
+            echo "<li><a href='web/historial.php'>Historial</a></li>";
+            echo "<li><a href='web/logout.php'>Cerrar sesion</a></li>";
+            echo "</ul>";
+            echo "</li>";
+          } else {
+            echo "<li class='drop-down'><a href='#'>Login</a>";
+            echo "<ul>";
+            echo "<li><a href='web/registro.php'>Registrarse</a></li>";
+            echo "<li><a href='web/login.php'>Iniciar sesion</a></li>";
+            echo "</ul>";
+            echo "</li>";
+          }
+          ?>
         </ul>
       </nav><!-- .nav-menu -->
 
